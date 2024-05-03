ea5d4db564f366c20ae89403c5c65e49b065a0b6
ip1259 <ip1259@gmail.com>
2024-05-03 16:10:26 +0800

    刪除json儲存物件方式改由joblib處理

--- LawDataProcessor.py
+++ LawDataProcessor.py
@@ -1,19 +1,18 @@
 import datetime
-import json, joblib
+import joblib
 from typing import TextIO, Union
+import LawDataExceptionHandler
 
 
 class Article:
     """條文物件，主要會以List的方式用在法律物件內"""
 
-    def __init__(self, article_law_name: str = "", article_num: str = "", article_content: str = "", article_embedding: list[float] = None):
+    def __init__(self, article_law_name: str = "", article_num: str = "", article_content: str = "",
+                 article_embedding: list[float] = None):
         self.article_law_name = article_law_name
         self.article_number = article_num
         self.article_content = article_content
         self.article_embedding = article_embedding
-
-    def dump_to_json(self) -> Union[str, None]:
-        return json.dumps(self.dict(), indent=4, ensure_ascii=False)
 
     def dict(self):
         return {"ArticleNumber": self.article_number,
@@ -34,52 +33,6 @@ class LawData:
         self.law_effective_date = datetime.date(2000, 1, 1)
         self.law_articles: list[Article] = []
 
-    def get_law_json_data(self, law_json_file: TextIO = None, law_name=""):
-        """Get Law Data from json file"""
-        temp = json.load(law_json_file)
-        try:  # 代表讀取的是全部的法條
-            for law in temp['Laws']:
-                if law['LawName'] == law_name:
-                    self.law_name = law['LawName']
-                    self.law_level = law['LawLevel']
-                    try:
-                        self.law_modified_date = datetime.datetime.strptime(law['LawModifiedDate'], "%Y%m%d")
-                    except:
-                        self.law_modified_date = None
-                    try:
-                        self.law_effective_date = datetime.datetime.strptime(law['LawEffectiveDate'], "%Y%m%d")
-                    except:
-                        self.law_effective_date = None
-                    for article in law['LawArticles']:
-                        if article['ArticleType'] == "A":
-                            self.law_articles.append(Article(article_law_name=law_name,
-                                                             article_num=article['ArticleNo'],
-                                                             article_content=article['ArticleContent']))
-                        elif article['ArticleType'] == "C":
-                            self.law_articles.append(Article(article_law_name=law_name,
-                                                             article_num=article['ArticleNo'],
-                                                             article_content=article['ArticleContent']))
-        except KeyError as ke:  # 代表讀取的是嵌入過的法條檔
-            law = temp
-            if law['LawName'] == law_name:
-                self.law_name = law['LawName']
-                self.law_level = law['LawLevel']
-                try:
-                    self.law_modified_date = datetime.datetime.strptime(law['LawModifiedDate'], "%Y%m%d")
-                except:
-                    self.law_modified_date = None
-                try:
-                    self.law_effective_date = datetime.datetime.strptime(law['LawEffectiveDate'], "%Y%m%d")
-                except:
-                    self.law_effective_date = None
-                for article in law['LawArticles']:
-                    self.law_articles.append(Article(law_name,
-                                                     article['ArticleNumber'],
-                                                     article['ArticleContent'],
-                                                     article['ArticleEmbedding']
-                                                     )
-                                             )
-
     def dict(self) -> dict:
         articles = []
         for article in self.law_articles:
@@ -95,9 +48,6 @@ class LawData:
                 "LawModifiedDate": modDate,
                 "LawEffectiveDate": effectiveDate,
                 "LawArticles": articles}
-
-    def dump_to_json(self) -> Union[str, None]:
-        return json.dumps(self.dict(), indent=4, ensure_ascii=False)
 
     def get_law_name(self):
         return self.law_name
@@ -168,25 +118,12 @@ def analyze_chapter(chp_str: str):
     return result
 
 
-def load_data(law_name: str) -> Union[LawData, None]:
-    try:
-        with open("LawData/" + str(law_name) + ".json", 'r', encoding="utf-8") as f:
-            tmp = LawData()
-            tmp.get_law_json_data(f, law_name)
-            return tmp
-    except FileNotFoundError as e_noFile:
-        try:
-            with open("LawData/ChLaw.json", 'r', encoding="utf-8-sig") as f:
-                tmp = LawData()
-                tmp.get_law_json_data(f, law_name)
-                from AIProcessor import embedding_all_articles
-                embedding_all_articles(tmp)
-                return tmp
-        except Exception as e:
-            print(e)
-            return None
+def save_law_data(lawdata: LawData):
+    if lawdata.law_name != "":
+        joblib.dump(lawdata, filename="LawData/" + lawdata.law_name + ".ld")
+    else:
+        raise LawDataExceptionHandler.BlankLawDataException
 
 
-def save_data(data: LawData):
-    with open("LawData/" + str(data.law_name + ".json"), 'w', encoding="utf-8") as fp:
-        json.dump(data.dict(), fp, ensure_ascii=False, indent=4)
+def load_law_data(lawname: str):
+    return joblib.load(filename="LawData/" + lawname + ".ld")
